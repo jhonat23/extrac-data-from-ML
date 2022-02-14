3f71e7321a1b38cfdb742dd909a387e9de8bef4d
jhonat23 <jhonat23@gmail.com>
2022-02-14 17:33:27 -0500

    First version

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # extrac-data-from-ML
-Scrip for extrac tendencies in "electronica__audio_y_video" page
+Script for extract tendencies in "electronica__audio_y_video" page from Mercado Libre
 Hi! If you want to use this script, please note the following:
 - Be sure that you have all the libraries required
+- Check your url due to country
+- Output file is in.xlsx format. Note that every time code runs the file will be erased and created with new data included.
 - The code is not optimized, feel free to make changes and commit then
 - An last: THANKS FOR USE MY SCRIPT :D
--- extracdatafromML.py
+++ extracdatafromML.py
@@ -1,5 +1,5 @@
 import urllib.request, urllib.parse, urllib.error
-import BeautifulSoup
+from bs4 import BeautifulSoup
 import ssl, sys
 import pandas as pd
 from datetime import datetime
@@ -14,46 +14,44 @@ lst=list()
 lstt=list()
 t=datetime.today().strftime('%Y-%m-%d')
 
-while True:
-    print('working...')
-    url='https://tendencias.mercadolibre.com.co/1000-electronica__audio_y_video'#check the specific link to your country
-    html = urllib.request.urlopen(url, context=ctx).read()
-    soup = BeautifulSoup(html, "html.parser")
-    tags=soup('a')
+print('working...')
+url='https://tendencias.mercadolibre.com.co/1000-electronica__audio_y_video'#check the specific link to your country
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+tags=soup('a')
 
-    #Extracting all url in main url
-    for tag in tags:
-        print(tag.get('href', None))
-        x=tag.get('href', None)
-        lst.append(str(x))
+#Extracting all url in main url
+for tag in tags:
+    #print(tag.get('href', None))
+    x=tag.get('href', None)
+    lst.append(str(x))
 
-    #Shorting the list
-    for link in lst:
-        if ('listado.mercadolibre'in link
-        or 'electronica.mercadolibre' in link
-        or 'vehiculos.mercadolibre' in link
-        or 'computacion.mercadolibre' in link
-        or 'celulares.mercadolibre' in link
-        or 'carros.mercadolibre' in link):
-            f=link.rfind('/')
-            lstt.append(link[f+1:])
+#Shorting the list
+for link in lst:
+    if ('listado.mercadolibre'in link
+    or 'electronica.mercadolibre' in link
+    or 'vehiculos.mercadolibre' in link
+    or 'computacion.mercadolibre' in link
+    or 'celulares.mercadolibre' in link
+    or 'carros.mercadolibre' in link
+    or 'deportes.mercadolibre' in link):
+        f=link.rfind('/')
+        lstt.append(link[f+1:])
 
-    for linka in lstt:
-        linka.replace("-","")
+for linka in lstt:
+    linka.replace("-","")
 
-    #check if all item are included (min 40)
-    print('length of list:')
-    print(len(lstt))
+#check if all item are included (min 40)
+print('length of list:')
+print(len(lstt))
 
-    #preparing data to get in Excel
-    data=pd.DataFrame(lstt,columns=[t])
-    print('date of list:')
-    print(t)
+#preparing data to get in Excel
+data=pd.DataFrame(lstt,columns=[t])
+print('date of list:')
+print(t)
 
-    # reading de seed file and creating final list
-    datai=pd.read_excel('Informe_ted_ML.xlsx') #save in same folder of scrip
-    dataf=pd.concat([data,datai], axis=1)
-    dataf.to_excel('Informe_ted_ML.xlsx')
-    #dataf
-    break
-print('Done')
+# reading de seed file and creating final list
+datai=pd.read_excel('Output.xlsx') #save in same folder of script
+dataf=pd.concat([data,datai], axis=1)
+dataf.to_excel('Output.xlsx')
+#dataf
